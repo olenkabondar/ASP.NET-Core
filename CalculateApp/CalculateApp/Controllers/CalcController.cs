377b0e6da2a901dbe5b376e915993cdb696cd6db
Olena Bondar <olenabondar.ua@gmail.com>
2025-08-25 17:19:57 +0300

    "commit task2 lesson2 CalculateApp"

---
+++ CalculateApp/CalculateApp/Controllers/CalcController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CalculateApp.Controllers
+{
+    public class CalcController : Controller
+    {
+        public IActionResult Index()
+        {
+            return Content("use one of variant (add, sub, mul, div) like https://localhost/Calc/Add/20/10");
+        }
+        public IActionResult Add(int a, int b)
+        {
+            return Content((a + b).ToString());
+        }
+
+        public IActionResult Sub(int a, int b)
+        {
+            return Content((a - b).ToString());
+        }
+
+        public IActionResult Mul(int a, int b)
+        {
+            return Content((a * b).ToString());
+        }
+
+        public IActionResult Div(int a, int b)
+        {
+            if (b == 0)
+                return Content("Division by zero!");
+            return Content((a / b).ToString());
+        }
+    }
+}
---
+++ CalculateApp/CalculateApp/Program.cs
@@ -0,0 +1,25 @@
+﻿var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddControllersWithViews();
+
+var app = builder.Build();
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+app.UseRouting();
+app.UseAuthorization();
+
+app.UseEndpoints(endpoints =>
+{
+    // Маршрут для калькулятора з двома параметрами
+    endpoints.MapControllerRoute(
+        name: "Calc",
+        pattern: "Calc/{action}/{a}/{b}",
+        defaults: new { controller = "Calc" });
+
+    endpoints.MapControllerRoute(
+        name: "Default",
+        pattern: "{controller=Calc}/{action=Index}/{id?}");
+});
+
+app.Run();
