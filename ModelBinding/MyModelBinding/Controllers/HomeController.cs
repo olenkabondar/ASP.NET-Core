81bbd9d8906c37cc1a07741566d7f485e325633b
Olena Bondar <olenabondar.ua@gmail.com>
2025-09-08 09:54:38 +0300

    commit hw Model Binding

---
+++ ModelBinding/MyModelBinding/Controllers/HomeController.cs
@@ -0,0 +1,25 @@
+﻿using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using MyModelBinding.Models;
+
+namespace MyModelBinding.Controllers
+{
+    public class HomeController : Controller
+    {
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return View(new MyFormModel()); // передаємо порожню модель
+        }
+
+        [HttpPost]
+        public IActionResult Index(MyFormModel model)
+        {
+            Debug.WriteLine($"First: {model.First}");
+            Debug.WriteLine($"Second: {model.Second}");
+            Debug.WriteLine($"Count: {model.Count}");
+
+            return View(); // повертаємо модель назад у View
+        }
+    }
+}
---
+++ ModelBinding/MyModelBinding/Models/MyFormModel.cs
@@ -0,0 +1,9 @@
+﻿namespace MyModelBinding.Models
+{
+    public class MyFormModel
+    {
+        public string First { get; set; }
+        public string Second { get; set; }
+        public int Count { get; set; }
+    }
+}
